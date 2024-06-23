462833f153fb6110393dc0ea92ef93c3d81fac34
Olumide Adenigba <Olumideadenigba@gmail.com>
2024-06-23 01:04:18 +0100

    render fix0

--- src/app.js
+++ src/app.js
@@ -13,18 +13,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files (HTML and CSS)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src" , "public")));
 
 // Routes
 app.use("/", userRoutes);
 
-// Log the paths for debugging
-console.log('Serving static files from:', path.join(__dirname, 'public'));
-console.log('Serving index.html from:', path.join(__dirname, 'public', 'index.html'));
-
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "src" , "public", "index.html"));
 });
 
 //catch all route
